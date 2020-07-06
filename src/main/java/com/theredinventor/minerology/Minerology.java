c04c3728424d7a8376b3a0961e3fee579f619ec5
pikachu99 <stinky@email.com>
2020-07-05 22:44:38 -0500

    Add files via upload

--- src/main/java/com/theredinventor/minerology/Minerology.java
+++ src/main/java/com/theredinventor/minerology/Minerology.java
@@ -1,17 +1,21 @@
 package com.theredinventor.minerology;
 
 import com.theredinventor.minerology.util.RegistryHandler;
+import com.theredinventor.minerology.world.OreGeneration;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod("minerology")
+@Mod.EventBusSubscriber(modid = Minerology.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Minerology
 {
     private static final Logger LOGGER = LogManager.getLogger();
@@ -29,6 +33,11 @@ public class Minerology
     private void setup(final FMLCommonSetupEvent event) { }
 
     private void doClientStuff(final FMLClientSetupEvent event) { }
+
+    @SubscribeEvent
+    public static void loadCompleteEvent(FMLLoadCompleteEvent event){
+        OreGeneration.generationOre();
+    }
 
     public static final ItemGroup TAB = new ItemGroup("minerology_ores"){
         @Override
